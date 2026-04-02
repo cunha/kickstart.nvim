39cfca980ebb986d261bf7fba73d9f52f689daa7
Italo Cunha <cunha@dcc.ufmg.br>
2026-04-02 14:08:23 -0300

    feat: add bufferline, drop avante

--- init.lua
+++ init.lua
@@ -213,6 +213,9 @@ vim.keymap.set('n', '<A-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<A-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<A-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { desc = 'Show previous buffer' })
+vim.keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Show next buffer' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -647,6 +650,20 @@ require('lazy').setup({
 
         copilot = {},
         rust_analyzer = {},
+        -- rust_analyzer = {
+        --   cmd = vim.lsp.rpc.connect '.lspmux.sock',
+        --   -- When using unix domain sockets, use something like:
+        --   --cmd = vim.lsp.rpc.connect("/path/to/lspmux.sock"),
+        --   settings = {
+        --     ['rust-analyzer'] = {
+        --       lspMux = {
+        --         version = '1',
+        --         method = 'connect',
+        --         server = 'rust-analyzer',
+        --       },
+        --     },
+        --   },
+        -- },
         pyright = {},
       }
 
@@ -934,88 +951,7 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'yetone/avante.nvim',
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
-    build = vim.fn.has 'win32' ~= 0 and 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false' or 'make',
-    event = 'VeryLazy',
-    version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
-    opts = {
-      -- add any opts here
-      -- this file can contain specific instructions for your project
-      instructions_file = 'AGENTS.md',
-      -- for example
-      provider = 'zai',
-      providers = {
-        zai = {
-          __inherited_from = 'openai',
-          endpoint = 'https://api.z.ai/api/coding/paas/v4',
-          model = 'GLM-5',
-          timeout = 30000, -- Timeout in milliseconds
-          api_key_name = 'AVANTE_ZAI_API_KEY',
-        },
-        claude = {
-          endpoint = 'https://api.anthropic.com',
-          model = 'claude-sonnet-4-20250514',
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
-        },
-        moonshot = {
-          endpoint = 'https://api.moonshot.ai/v1',
-          model = 'kimi-k2-0711-preview',
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 32768,
-          },
-        },
-      },
-    },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      --- The below dependencies are optional,
-      -- 'nvim-mini/mini.pick', -- for file_selector provider mini.pick
-      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-      -- 'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-      -- 'ibhagwan/fzf-lua', -- for file_selector provider fzf
-      -- 'stevearc/dressing.nvim', -- for input provider dressing
-      -- 'folke/snacks.nvim', -- for input provider snacks
-      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-      -- 'zbirenbaum/copilot.lua', -- for providers='copilot'
-      -- {
-      --   -- support for image pasting
-      --   'HakonHarnes/img-clip.nvim',
-      --   event = 'VeryLazy',
-      --   opts = {
-      --     -- recommended settings
-      --     default = {
-      --       embed_image_as_base64 = false,
-      --       prompt_for_file_name = false,
-      --       drag_and_drop = {
-      --         insert_mode = true,
-      --       },
-      --       -- required for Windows users
-      --       use_absolute_path = true,
-      --     },
-      --   },
-      -- },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
-    },
-  },
+  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
